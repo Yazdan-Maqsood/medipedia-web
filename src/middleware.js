0273ae49409fbdb9185e272e2ccea51d567e0a1e
Ahad Ali <100362508+ahadali0500@users.noreply.github.com>
2024-02-22 23:38:44 -0800

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -3,42 +3,38 @@ import { NextResponse } from 'next/server';
 export async function middleware(request) {
   let cookie = request.cookies.get('next-auth.session-token');
 
-  // Ensure the list includes all the routes you want to protect or require authentication
-  const authenticatedRoutes = ['/login', '/register', '/forgot-password'];
+  const authenticatedRoutes = ['/login', '/register','/forgot-password'];
   const nonAuthRoutesPatterns = [
-    '/apply-code',
-    '/book-code',
-    '/book-price',
-    '/change-email',
-    '/change-password',
-    '/change-phone-number',
-    '/change-profile-name',
-    '/guide',
-    '/hints',
-    '/profile',
-    '/quiz',
-    '/saved-quiz',
-    '/search',
-    '/success',
-];
+      '/apply-code',
+      '/book-code',
+      '/book-price',
+      '/change-email',
+      '/change-password',
+      '/change-phone-number',
+      '/change-profile-name',
+      '/guide',
+      '/hints',
+      '/profile',
+      '/quiz',
+      '/saved-quiz',
+      '/search',
+      '/success',
+  ];
 
+  // Use nextUrl.pathname for Next.js versions that support it
   const pathname = request.nextUrl.pathname;
-  const basePath = pathname.split('/')[1]; // This assumes your paths are directly under the root
 
-  // Debug: Log values for troubleshooting (remove in final code for production)
-  console.log(`Pathname: ${pathname}, BasePath: ${basePath}, Cookie: ${cookie}`);
+  // Logic for determining if the pathname matches authenticated or non-authenticated routes
+  const basePath = pathname.split('/')[1]; // Gets the first segment of the path
 
   const isNonAuthRoute = nonAuthRoutesPatterns.some(route => `/${basePath}`.startsWith(route));
-  const isAuthRoute = authenticatedRoutes.includes(pathname); // Adjusted for exact match
+  const isAuthRoute = authenticatedRoutes.some(route => `/${basePath}`.startsWith(route));
 
   if (cookie && isAuthRoute) {
-    // Redirect authenticated users away from auth routes
-    return NextResponse.redirect(new URL('/', request.nextUrl));
-  } else if (!cookie && !isNonAuthRoute) {
-    // Redirect unauthenticated users to login from non-auth routes
-    return NextResponse.redirect(new URL('/login', request.nextUrl));
+      return NextResponse.redirect(new URL('/', request.nextUrl));
+  } else if (!cookie && isNonAuthRoute) {
+      return NextResponse.redirect(new URL('/login', request.nextUrl));
   }
 
-  // Proceed with request for all other cases
   return NextResponse.next();
 }
