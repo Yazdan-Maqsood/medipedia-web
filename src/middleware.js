72b05b4a40e40c61dd9ff9b51f0ae289b0718816
Ahad Ali <100362508+ahadali0500@users.noreply.github.com>
2024-03-18 08:24:07 -0700

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,45 +1,41 @@
-// This example assumes you're using the new Middleware (file-based routing) in Next.js 14.
+import { NextResponse } from 'next/server';
 
-import { getToken } from "next-auth/jwt";
-import { NextResponse } from "next/server";
+export async function middleware(request) {
+  //let cookie = request.cookies.get('next-auth.session-token');
+  let cookie = request.cookies.get('__Secure-next-auth.session-token');
 
-// This is your secret from NextAuth configuration. Ensure it matches.
-const secret = process.env.NEXTAUTH_SECRET;
+  const authenticatedRoutes = ['/login', '/register','/forgot-password'];
+  const nonAuthRoutesPatterns = [
+      '/apply-code',
+      '/book-code',
+      '/book-price',
+      '/change-email',
+      '/change-password',
+      '/change-phone-number',
+      '/change-profile-name',
+      '/guide',
+      '/hints',
+      '/profile',
+      '/quiz',
+      '/saved-quiz',
+      '/search',
+      '/success',
+  ];
 
-export async function middleware(req) {
-    const session = await getToken({ req, secret });
-    const authenticatedRoutes = ['/login', '/register', '/forgot-password'];
-    const nonAuthRoutesPatterns = [
-        '/apply-code',
-        '/book-code',
-        '/book-price',
-        '/change-email',
-        '/change-password',
-        '/change-phone-number',
-        '/change-profile-name',
-        '/guide',
-        '/hints',
-        '/profile',
-        '/quiz',
-        '/saved-quiz',
-        '/search',
-        '/success',
-    ];
-    // Use nextUrl.pathname for Next.js versions that support it
-    const pathname = req.nextUrl.pathname;
+  // Use nextUrl.pathname for Next.js versions that support it
+  const pathname = request.nextUrl.pathname;
 
-    // Logic for determining if the pathname matches authenticated or non-authenticated routes
-    const basePath = pathname.split('/')[1]; // Gets the first segment of the path
+  // Logic for determining if the pathname matches authenticated or non-authenticated routes
+  const basePath = pathname.split('/')[1]; // Gets the first segment of the path
 
-    const isNonAuthRoute = nonAuthRoutesPatterns.some(route => `/${basePath}`.startsWith(route));
-    const isAuthRoute = authenticatedRoutes.some(route => `/${basePath}`.startsWith(route));
+  const isNonAuthRoute = nonAuthRoutesPatterns.some(route => `/${basePath}`.startsWith(route));
+  const isAuthRoute = authenticatedRoutes.some(route => `/${basePath}`.startsWith(route));
 
-    if (session && isAuthRoute) {
-        return NextResponse.redirect(new URL('/', req.nextUrl));
-    } else if (!session && isNonAuthRoute) {
-        return NextResponse.redirect(new URL('/login', req.nextUrl));
-    }
+  if (cookie && isAuthRoute) {
+      return NextResponse.redirect(new URL('/', request.nextUrl));
+  } else if (!cookie && isNonAuthRoute) {
+      return NextResponse.redirect(new URL('/login', request.nextUrl));
+  }
 
-    // Continue to the requested page if session exists or if it's a public path
-    return NextResponse.next();
+  return NextResponse.next();
 }
