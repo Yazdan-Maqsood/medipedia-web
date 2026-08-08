edfac175503a2ae4d421344404c46a58ea2a6890
Yazdan-Maqsood <yazdanmaqsood2@gmail.com>
2026-08-08 16:26:11 +0500

    fixing histiory

--- src/app/history/page.jsx
+++ src/app/history/page.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import Link from 'next/link'
+import React from 'react';
+import Link from 'next/link';
 import { apiUrl } from '../config/constant';
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth/next";
+import { redirect } from 'next/navigation'; // <-- Added this
 import Data from './Data';
-
 
 export const metadata = {
     title: "History - Medipedia",
-  };
+};
 
 export default async function page() {
     const datas = await getServerSession(authOptions);
-    // const data = await getData(datas.user.id)
+
+    // ✅ Agar user login NAHI hai, toh seedha login page par bhej dein
+    if (!datas) {
+        redirect('/login');
+    }
+
+    // ✅ Agar login hai, toh history page render karein
     return (
         <section className="shopping-cart-area ptb-50">
             <h1 className="text-center">History</h1>
             <br />
             <Data user_id={datas.user.id} />
         </section>
-
-    )
+    );
 }
