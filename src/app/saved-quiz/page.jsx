c2c85d51dc9020a420b4472f70576a57578201a0
Yazdan-Maqsood <yazdanmaqsood2@gmail.com>
2026-08-08 16:29:06 +0500

    testing

--- src/app/saved-quiz/page.jsx
+++ src/app/saved-quiz/page.jsx
@@ -2,22 +2,26 @@ import React from "react";
 import { apiUrl } from '../config/constant';
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth/next";
+import { redirect } from 'next/navigation'; // <-- Added redirect
 import Data from "./Data";
 
 export const metadata = {
   title: "Saved Quiz - Medipedia",
 };
 
-
 export default async function page() {
   const datas = await getServerSession(authOptions);
-  // const data = await getData(datas.user.id)
+  
+  // ✅ Agar user login NAHI hai, toh seedha login page par bhej dein
+  if (!datas) {
+      redirect('/login');
+  }
   
   return (
     <section className="shopping-cart-area ptb-50">
       <h1 className="text-center">Saved Quiz</h1>
       <br />
-      <Data user_id={datas.user.id} ></Data>
+      <Data user_id={datas.user.id}></Data>
     </section>
   );
 }
