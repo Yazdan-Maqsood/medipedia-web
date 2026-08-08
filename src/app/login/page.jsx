56fde0c9ed0c72ec847e4353cef33c4880d05ac6
Yazdan-Maqsood <yazdanmaqsood2@gmail.com>
2026-08-08 16:15:02 +0500

    hopefully it is fixed

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,6 +1,6 @@
-import Link from 'next/link'
-import React from 'react'
-import Form from '../login/Form'
+import Link from 'next/link';
+import React from 'react';
+import Form from '../login/Form';
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth/next";
 import { redirect } from 'next/navigation';
@@ -10,45 +10,29 @@ export const metadata = {
 };
 
 export default async function page() {
-
     const data = await getServerSession(authOptions);
+
+    // 1. Agar user login hai toh seedha Home page par bhej dein
     if (data) {
-        return (
-            <section className="sign-up-section ptb-50">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-3 col-md-3 ps-0" />
-                        <div className="col-lg-6 col-md-6 ps-0">
-                             
-                            <h2>You cannot access this page!</h2>
-                            <br></br>
-                            {/* <p>Fill your email and password to login.</p> */}
-                        </div>
-                        <div className="col-lg-3 col-md-3 ps-0" />
-                    </div>
-                </div>
-            </section>
-
-        )
-    } else {
-        return (
-            <section className="sign-up-section ptb-50">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-3 col-md-3 ps-0" />
-                        <div className="col-lg-6 col-md-6 ps-0">
-                            <div className="sign-up-form">
-                                <h2>Welcome Back </h2>
-                                <p>Fill your email and password to login.</p>
-                                <Form></Form>
-                            </div>
-                        </div>
-                        <div className="col-lg-3 col-md-3 ps-0" />
-                    </div>
-                </div>
-            </section>
-
-        )
-
+        redirect('/');
     }
+
+    // 2. Agar login nahi hai, toh seedha Form dikhayen (extra 'else' ki zaroorat nahi)
+    return (
+        <section className="sign-up-section ptb-50">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-3 col-md-3 ps-0" />
+                    <div className="col-lg-6 col-md-6 ps-0">
+                        <div className="sign-up-form">
+                            <h2>Welcome Back</h2>
+                            <p>Fill your email and password to login.</p>
+                            <Form />
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-md-3 ps-0" />
+                </div>
+            </div>
+        </section>
+    );
 }
